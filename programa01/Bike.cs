146390dbc6670f436b55ac1b1660636e05304eb5
guilhermeGiovanini <guilhermegiovanini1403@gmail.com>
2026-08-19 15:24:43 -0300

    feat: Criando as declarações do projeto

---
+++ programa01/Bike.cs
@@ -0,0 +1,11 @@
+﻿namespace programa01
+{
+    class Bike
+    {
+        //declarações
+        public string? Modelo {  get; set; }
+        public double Bateria { get; set; }
+        public double PesoCiclista { get; set; }
+        public int RecargaMes { get; set; }
+    }
+}
