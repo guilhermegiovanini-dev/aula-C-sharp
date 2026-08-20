467ce1f32b464c279e50373933cae3e0a9fab055
Guilherme Giovanini <guilhermegiovanini1403@gmail.com>
2026-08-20 17:00:54 -0300

    feat: Codificando a classe bike propriedades e metodos

--- programa01/Bike.cs
+++ programa01/Bike.cs
@@ -3,9 +3,33 @@
     class Bike
     {
         //declarações
-        public string? Modelo {  get; set; }
-        public double Bateria { get; set; }
+        public string? Modelo {  get; set; } //= string.Empty ou ?
+        public double Bateria { get; set; } 
         public double PesoCiclista { get; set; }
         public int RecargaMes { get; set; }
+
+
+        //métodos (Ações)
+        public double CalcularAutonomia()
+        {
+            double autonomiaBase = Bateria / 15;
+            bool peso = PesoCiclista > 90;
+            // se o peso do ciclista for maior que 90kg, redus a autonomia para 15%, senão autonomia normal
+            return peso ? (autonomiaBase * 0.05) : autonomiaBase;
+        }
+        public double CalcularConsumoMensal()
+        {
+            double cargaPorKm = Bateria / 1000;
+            return cargaPorKm * RecargaMes;
+        }
+        public double CalcularCustoMensal() { 
+            const double PRECO = 0.80;
+            return CalcularConsumoMensal() * PRECO;
+        }
+        public bool BikeEconomica()
+        {
+            return (CalcularCustoMensal() < 15) || (CalcularAutonomia() > 40);
+        }
+
     }
 }
